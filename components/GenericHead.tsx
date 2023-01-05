c0442b7d4942fbdbdece0e1a1ba59735d4edb33a
srslafazan <srslafazan@gmail.com>
2023-01-04 22:16:19 -0800

    Created content

---
+++ components/GenericHead.tsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export const GenericHead = () => (
+  <Head>
+    <title>Stactica</title>
+    <meta name="description" content="Stactica is the a software company." />
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
+);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,46 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "../styles/Modal.css";
+import type { AppProps } from "next/app";
+import { useState } from "react";
+
+import { AiOutlineClose } from "react-icons/ai";
+import { ModalContext } from "../context/modal";
+
+const Modal = ({ visible = false, close = () => {}, children = [] }: any) => {
+  if (!visible) return null;
+
+  return (
+    <div className="modal">
+      <AiOutlineClose className="menu-close" onClick={close} style={{}} />
+      {children}
+    </div>
+  );
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [modalContext, setModalContext] = useState({
+    content: null,
+    update: () => {},
+  });
+  return (
+    <ModalContext.Provider
+      value={{
+        ...modalContext,
+        update: setModalContext,
+        close: () => setModalContext({ ...modalContext, content: null }),
+      }}
+    >
+      <Modal
+        visible={!!modalContext.content}
+        close={() => {
+          setModalContext({ ...modalContext, content: null });
+        }}
+      >
+        {modalContext.content}
+      </Modal>
+      {/* <Toaster toastOptions={{ className: "rw-toast", duration: 6000 }} /> */}
+      <Component {...pageProps} />
+    </ModalContext.Provider>
+  );
+  return;
 }
